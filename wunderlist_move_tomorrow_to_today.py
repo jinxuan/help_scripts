64820f9ae6371f44587fa9f69fc96dd953629ee1
Jinxuan Wu <jinxuanw@asu.edu>
2016-07-11 17:50:16 -0700

    add move from tomorrow to today

---
+++ wunderlist_move_tomorrow_to_today.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import wunderpy2
+api = wunderpy2.WunderApi()
+client = api.get_client("4571f8d62d1f9ad3a52eac32cfefeaa8191ed750ef017475d2429269bcf3", "a2fcef806cc3fabd1841")
+tomorrow_to_do_name = []
+tomorrow_to_do_id_revision = []
+for item in client.get_tasks(244025185):
+	tomorrow_to_do_id_revision.append((item['id'], item['revision']))
+	tomorrow_to_do_name.append(item['title'])
+
+for item in tomorrow_to_do_id_revision:
+	client.delete_task(item[0], item[1])
+
+for item in tomorrow_to_do_name:
+	client.create_task(197330953, item)

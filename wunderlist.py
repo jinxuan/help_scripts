ab8a0f4bbb991a4c8da5b1d7c2b62e69efb60df6
Jinxuan Wu <jinxuanw@asu.edu>
2016-07-13 10:07:36 -0700

    refactor, use variable instead of hard coded.

--- wunderlist.py
+++ wunderlist.py
@@ -3,11 +3,7 @@
 import wunderpy2
 api = wunderpy2.WunderApi()
 client = api.get_client("4571f8d62d1f9ad3a52eac32cfefeaa8191ed750ef017475d2429269bcf3", "a2fcef806cc3fabd1841")
-client.create_task(197330953, "面试， 算法题， leetcode")
-client.create_task(197330953, "锻炼身体")
-client.create_task(197330953, "开源项目， 源码阅读")
-client.create_task(197330953, "技术书")
-client.create_task(197330953, "钢琴")
-client.create_task(197330953, "单词")
-client.create_task(197330953, "数学")
-client.create_task(197330953, "杂书")
+tasklist = ["面试， 算法题， leetcode", "锻炼身体", "开源项目， 源码阅读", "技术书", "钢琴", "单词", "数学", "杂书", "论文"]
+to_do_list_id = 197330953
+for task in tasklist:
+    client.create_task(to_do_list_id, task)

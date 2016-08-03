09fd6cfcc54d9f9a46bb1266ec8d53e674d3f970
Jinxuan Wu <jinxuanw@asu.edu>
2016-08-03 09:35:04 -0700

    change key

--- wunderlist.py
+++ wunderlist.py
@@ -2,10 +2,10 @@
 #  -*- coding: utf-8 -*-
 import wunderpy2
 api = wunderpy2.WunderApi()
-client = api.get_client("4571f8d62d1f9ad3a52eac32cfefeaa8191ed750ef017475d2429269bcf3", "a2fcef806cc3fabd1841")
+client = api.get_client("7196edc056e863b88b1d217ffe87fdde59d6800211603c4ae9c009c98872", "a2fcef806cc3fabd1841")
 tasklist = ["早上单词", "开源项目， 源码阅读:", "阅读课", "数学书：", \
 			"中文杂书：", "英文杂书", "论文：", "技术书", "钢琴", "乐谱 sight reading", \
-			"面试， 算法题，leetcode", "锻炼身体",   "晚上单词", "水果, 苹果", "六味地黄丸","上传单词"]
+			"面试， 算法题，leetcode", "锻炼身体",   "晚上单词", "水果, 苹果", "六味地黄丸","上传新认识的单词到 shanbay"]
 to_do_list_id = 197330953
 for task in tasklist:
     client.create_task(to_do_list_id, task)
--- wunderlist_move_tomorrow_to_today.py
+++ wunderlist_move_tomorrow_to_today.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import wunderpy2
 api = wunderpy2.WunderApi()
-client = api.get_client("4571f8d62d1f9ad3a52eac32cfefeaa8191ed750ef017475d2429269bcf3", "a2fcef806cc3fabd1841")
+client = api.get_client("7196edc056e863b88b1d217ffe87fdde59d6800211603c4ae9c009c98872", "a2fcef806cc3fabd1841")
 tomorrow_to_do_name = []
 tomorrow_to_do_id_revision = []
 for item in client.get_tasks(244025185):
